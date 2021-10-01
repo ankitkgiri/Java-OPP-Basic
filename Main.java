e1d31f1b77cdd4fd5449917d3666324130b18a7e
Ankit Kumar <55329837+ankitkgiri@users.noreply.github.com>
2021-10-01 11:03:17 +0000

    Few Changes.

--- Main.java
+++ Main.java
@@ -3,11 +3,19 @@ class Main {
     Employee employeeOne = new Employee("John Doe", "Delhi", 35000, 25);
     Employee employeeTwo = new Employee("Don Joe", "Mumbai", 15000, 21);
 
-    employeeTwo.raiseSalary(50);
-    
+    System.out.println();
+    System.out.println("Employee Details: ");
+
     employeeOne.display();
     System.out.println();
     employeeTwo.display();
+    System.out.println();
+    System.out.println("Salary raise to empoyeeTwo by 40%");
+    employeeTwo.raiseSalary(40);
+    System.out.println();
+    System.out.println("Salary After Raise: ");
 
+    System.out.println(employeeOne.getName() + ": " + employeeOne.getSalary());
+    System.out.println(employeeTwo.getName() + ": " + employeeTwo.getSalary());
   }
-}
+} 
